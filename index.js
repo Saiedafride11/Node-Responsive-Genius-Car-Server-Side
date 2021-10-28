4d5f778d19aa8f885cf43cca8083adb2cf163d9b
Coders Key <designer.afride@gmail.com>
2021-10-28 02:09:38 -0700

    port added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const ObjectId = require('mongodb').ObjectId;
 // const bodyParser = require('body-parser')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.use(cors())
 // app.use(bodyParser.json())
